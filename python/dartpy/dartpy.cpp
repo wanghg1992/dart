9e7030cf7e850f66633090dd1fa69190dfbae2a6
Jeongseok Lee <jslee02@gmail.com>
2019-01-12 09:14:57 -0800

    Build dartpy on macOS

--- python/dartpy/dartpy.cpp
+++ python/dartpy/dartpy.cpp
@@ -5,25 +5,22 @@ namespace py = pybind11;
 namespace dart {
 namespace python {
 
+void dart_common(pybind11::module& m);
+void dart_math(pybind11::module& m);
+void dart_dynamics(pybind11::module& m);
+void dart_simulation(pybind11::module& m);
+void dart_utils(pybind11::module& m);
+
 PYBIND11_MODULE(dartpy, m)
 {
   py::module::import("numpy");
 
   m.doc() = "DART python bindings";
 
-  void dart_common(pybind11::module & m);
   dart_common(m);
-
-  void dart_math(pybind11::module & m);
   dart_math(m);
-
-  void dart_dynamics(pybind11::module & m);
   dart_dynamics(m);
-
-  void dart_simulation(pybind11::module & m);
   dart_simulation(m);
-
-  void dart_utils(pybind11::module & m);
   dart_utils(m);
 }
 
